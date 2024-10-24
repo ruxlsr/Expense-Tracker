ab7799a77db3588eaed0db2a6e29ad66fe3d742e
ruxlsr <russel.lontsi@facsciences-uy1.cm>
2024-10-24 23:35:02 +0100

    get the now localdate

--- src/org/expensetracker/model/ExpensesList.java
+++ src/org/expensetracker/model/ExpensesList.java
@@ -1,5 +1,6 @@
 package org.expensetracker.model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -11,22 +12,40 @@ public class ExpensesList {
         this.expenses = new ArrayList<Expense>();
     }
 
+    /**
+     * @return List<Expense>
+     */
     public List<Expense> getExpenses() {
         return expenses;
     }
 
+    /**
+     * @param expenses
+     */
     public void setExpenses(List<Expense> expenses) {
         this.expenses = expenses;
     }
 
-    public void addExpense(Expense expense) {
-        expenses.add(expense);
+    /**
+     * @param expense
+     */
+    public void addExpense(String description, int amount) {
+        LocalDate ld = LocalDate.now();
+        expenses.add(new Expense(expenses.getLast().getId() + 1, description, amount,
+                LocalDate.of(ld.getYear(), ld.getMonthValue(), ld.getDayOfMonth())));
     }
 
+    /**
+     * @param id
+     */
     public void deleteExpense(int id) {
         expenses.remove(id);
     }
 
+    /**
+     * @param id
+     * @param description
+     */
     public void updateDescription(int id, Optional<String> description) {
         description.ifPresent((value) -> {
             if (!value.isEmpty()) {
@@ -39,6 +58,10 @@ public class ExpensesList {
 
     }
 
+    /**
+     * @param id
+     * @param amount
+     */
     public void updateAmount(int id, Optional<String> amount) {
 
         amount.ifPresent((value) -> {
