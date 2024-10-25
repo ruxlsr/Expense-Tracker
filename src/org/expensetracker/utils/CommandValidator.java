f985c3c634e98fe4a6ce3d8fe43476f5dd30de20
ruxlsr <russel.lontsi@facsciences-uy1.cm>
2024-10-25 09:05:03 +0100

    check update arg feature

--- src/org/expensetracker/utils/CommandValidator.java
+++ src/org/expensetracker/utils/CommandValidator.java
@@ -1,7 +1,7 @@
 package org.expensetracker.utils;
 
 public class CommandValidator {
-    public static boolean validateAddingArguments(String arg1, String arg2, String arg3, String arg4) {
+    public static boolean checkAddingArgs(String arg1, String arg2, String arg3, String arg4) {
         if (arg1.isEmpty() || arg3.isEmpty()) {
             System.err.println("Syntax Error: Empty argument");
             return false;
@@ -21,6 +21,48 @@ public class CommandValidator {
                 && !arg3.equals("--amount")) {
             System.err.println("Invalid argument");
             return false;
+        }
+
+        return true;
+    }
+
+    public static boolean checkUpdatingArgs(String arg1, String arg2, String arg3, String arg4, String arg5,
+            String arg6) {
+        // Group arguments into pairs for easier processing
+        String[][] args = { { arg1, arg2 }, { arg3, arg4 }, { arg5, arg6 } };
+
+        for (String[] pair : args) {
+            if (pair[0].isEmpty()) {
+                System.err.println("Syntax Error: Empty argument");
+                return false;
+            }
+
+            if (pair[0].equals("--amount")) {
+                try {
+                    int amount = Integer.parseInt(pair[1]);
+                    if (amount < 0) {
+                        System.err.println("Invalid Amount value");
+                        return false;
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Invalid Amount value: Not a number");
+                    return false;
+                }
+            } else if (pair[0].equals("--id")) {
+                try {
+                    int id = Integer.parseInt(pair[1]);
+                    if (id < 0) {
+                        System.err.println("Invalid id value");
+                        return false;
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Invalid id value: Not a number");
+                    return false;
+                }
+            } else if (!pair[0].equals("--description")) {
+                System.err.println("Invalid argument: " + pair[0]);
+                return false;
+            }
         }
 
         return true;
